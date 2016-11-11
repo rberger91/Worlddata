5d6caa1ba89f0e5378ec0e23c693ede80cc96aec
rberger91 <robbinberger@gmail.com>
2016-11-11 23:16:20 +0100

    data subset gemakt
    Nu zitten de x-jes weer in  de kolomnamen

--- population growth.R
+++ population growth.R
@@ -1,6 +1,15 @@
 
-worldpopulation <- read.table("~//data files//API_SP.POP.TOTL_DS2_en_csv_v2.csv", sep = ",", skip = 4)
+worldpopulation <- read.table("~//data files//API_SP.POP.TOTL_DS2_en_csv_v2.csv", sep = ",", skip = 4, header = T)
 edit(head(worldpopulation[1:3,]))
 nrow(worldpopulation)
 ncol(worldpopulation)
+str(worldpopulation)
 
+
+countries <- levels(worldpopulation$Country.Name)
+
+
+#NL en Korea filteren
+nlzkdata <- subset(worldpopulation,worldpopulation$Country.Name == "Korea, Rep." |   worldpopulation$Country.Name == "Netherlands")
+t(nlzkdata)
+help(t)
