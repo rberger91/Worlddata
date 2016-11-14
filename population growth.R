6feb427674beefc2ca8b18336d305d46db3fbe8c
rberger91 <robbinberger@gmail.com>
2016-11-14 13:10:16 +0100

    nog steeds plotten =)

--- population growth.R
+++ population growth.R
@@ -16,16 +16,11 @@ nlzkdata
 colnames(nlzkdata)[5:62] <- substring(names(nlzkdata[5:62]),2) #werkt
 nlzkdata
 
-plot(t(nlzkdata$Country.Name == "Netherlands"))
-head(nlzkdata)
-
 install.packages("reshape2")
 library(reshape2)
-library("ggplot2")
-help(ggplot)
 
-
-nlzkdata <- melt(nlzkdata)
+nlzkdata <- melt(nlzkdata)  #zet de dataframe om naar long-format, i.p.v. wide
+names(nlzkdata) <- tolower(names(nlzkdata)) #haalt de hoofdlettersweg in kolomnamen
 head(nlzkdata)
-plot(nlzkdata$variable[nlzkdata$Country.Name=="Korea, Rep."],nlzkdata$value[nlzkdata$Country.Name=="Korea, Rep."]/1000, type = "n")
-help(plot)
+nlzkdata <- nlzkdata[,-2:-4]
+head(nlzkdata)
