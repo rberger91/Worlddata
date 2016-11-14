97c4b11f599ca8ab85aa29ccaffd18ab82ad6cf3
rberger91 <robbinberger@gmail.com>
2016-11-14 12:17:26 +0100

    Een sipele lijn plot proberen te krijgen uit de data
    eerst omgezet via een package naar long data, nu opzoek naar een manier
    op een simpel lijntje te krijgen.

--- population growth.R
+++ population growth.R
@@ -13,5 +13,19 @@ countries <- levels(worldpopulation$Country.Name)
 nlzkdata <- subset(worldpopulation,worldpopulation$Country.Name == "Korea, Rep." |   worldpopulation$Country.Name == "Netherlands")
 nlzkdata
 
-names(colnames(nlzkdata[5:62])) <- substring(names(nlzkdata[5:62]),2)
+colnames(nlzkdata)[5:62] <- substring(names(nlzkdata[5:62]),2) #werkt
 nlzkdata
+
+plot(t(nlzkdata$Country.Name == "Netherlands"))
+head(nlzkdata)
+
+install.packages("reshape2")
+library(reshape2)
+library("ggplot2")
+help(ggplot)
+
+
+nlzkdata <- melt(nlzkdata)
+head(nlzkdata)
+plot(nlzkdata$variable[nlzkdata$Country.Name=="Korea, Rep."],nlzkdata$value[nlzkdata$Country.Name=="Korea, Rep."]/1000, type = "n")
+help(plot)
