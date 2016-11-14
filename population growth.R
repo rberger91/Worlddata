2e5c463c0479ee8113e884fd23d8f9ede9bc4fac
rberger91 <robbinberger@gmail.com>
2016-11-14 14:31:23 +0100

    1e plot gelukt
    Erg basis en niet erg goed, maar het is een goed begin.

--- population growth.R
+++ population growth.R
@@ -11,16 +11,28 @@ countries <- levels(worldpopulation$Country.Name)
 
 #NL en Korea filteren
 nlzkdata <- subset(worldpopulation,worldpopulation$Country.Name == "Korea, Rep." |   worldpopulation$Country.Name == "Netherlands")
-nlzkdata
 
 colnames(nlzkdata)[5:62] <- substring(names(nlzkdata[5:62]),2) #werkt
-nlzkdata
 
 install.packages("reshape2")
 library(reshape2)
 
 nlzkdata <- melt(nlzkdata)  #zet de dataframe om naar long-format, i.p.v. wide
 names(nlzkdata) <- tolower(names(nlzkdata)) #haalt de hoofdlettersweg in kolomnamen
+nlzkdata <- nlzkdata[,-2:-6] #verweiderd ongebruikte kolommen
 head(nlzkdata)
-nlzkdata <- nlzkdata[,-2:-4]
-head(nlzkdata)
+
+#plot(nlzkdata$variable[nlzkdata$country.name=="Korea, Rep."],nlzkdata$value[nlzkdata$country.name=="Korea, Rep."]/1000000, type = "n", ylim = c(0,50))
+#lines(nlzkdata$variable[nlzkdata$country.name=="Netherlands"],nlzkdata$value[nlzkdata$country.name=="Netherlands"]/1000000)
+#plot(nlzkdata$country. ~ nlzkdata$value)
+
+help(matplot)
+matplot(nlzkdata$variable[nlzkdata$country.name == "Korea, Rep."],nlzkdata$value[nlzkdata$country.name == "Korea, Rep."]/1000, type = "l", ylim = c(0,55000),
+        col = "red", main = "national population growth", ylab = "size of population ( x 1000)", xlab = "years")
+matplot(nlzkdata$variable[nlzkdata$country.name == "Netherlands"],nlzkdata$value[nlzkdata$country.name == "Netherlands"]/1000, type = "l", add = T, 
+        col = "blue")
+help(legend)
+legend(x = 1960, y = 55000, legend = unique(nlzkdata$country.name), fill = c("red","blue"))
+
+# lines(nlzkdata$variable[nlzkdata$country.name=="Netherlands"],nlzkdata$value[nlzkdata$country.name=="Netherlands"]/1000)
+# help(lines)
